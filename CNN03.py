c66847a9c374c53d60cb6f18748b8243dc5c0b37
Mehrnoush Haghighat Kashani <59601818+Mehrnoushhk@users.noreply.github.com>
2020-07-09 23:56:46 +0430

    lets get into cnn

--- CNN03.py
+++ CNN03.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import datetime
 import mplfinance as mpl
+print(mpl.__version__)
 url= 'https://raw.githubusercontent.com/Mehrnoushhk/DeepLearning/master/GBPUSDm30.csv'
 data= pd.read_csv(url) 
 from DataCleaner import dataCleaner
@@ -10,7 +11,6 @@ from DataCleaner import localMax
 from DataCleaner import localMin
 dataMax, dataMaxValue= localMax(data, 4)
 dataMin, dataMinValue= localMin(data, 4)
-data['isMax']= dataMax
-data['maxValue']= dataMaxValue
-data['isMin']= dataMin
-data['minValue']= dataMinValue
+minMax= pd.DataFrame(list(zip(dataMinValue, dataMaxValue)), columns=['min', 'Max'])
+adp= mpl.make_addplot(minMax[len(minMax)-100:], type= 'scatter', markersize= 100)
+mpl.plot(data.iloc[data.shape[0]-100: data.shape[0],:], type= 'candle', style= 'charles', figscale= 6, addplot= adp)
