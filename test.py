f66457a4086c01846eaefa7535d83373630bcc80
Mehrnoush Haghighat Kashani <mehrnoosh.haghighat@gmail.com>
2020-11-04 23:55:45 +0330

    committed 110401

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
+import fileinput
 import pandas as pd
-import keras
-print('Test')
+inputData = []
+for line in fileinput.input(files='f6bdc3bab15c11ea.txt'):
+    inputData.append(line.strip())
+print(inputData)
+fileinput.close()
+testNumbers = int(inputData[0])
+matrix = []
+i= 1
+while i < len(inputData):
+    matrixSize = int(inputData[i])
+    tempList = []
+    for j in range(1, matrixSize+1):
+        tempList.append(inputData[i+j])
+    matrix.append(tempList)
+    i += matrixSize + 1
+print(matrix)
